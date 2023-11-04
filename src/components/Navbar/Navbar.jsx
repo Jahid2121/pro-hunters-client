6c82cb8cc2e72ee6152b8c8ae821e29cbf1af0bb
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-04 23:59:38 +0600

    Add font poppins

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import NavItems from "./NavItems";
 
 const Navbar = ({children}) => {
   return (
-    <div className="drawer">
+    <div className="drawer font-poppins">
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         {/* Navbar */}
@@ -32,7 +32,7 @@ const Navbar = ({children}) => {
               </svg>
             </label>
           </div>
-          <div className="flex-1 px-2 mx-2">Navbar Title</div>
+          <div className="flex-1 px-2 mx-2">ProHunters</div>
           <div className="flex-none hidden lg:block">
             <ul className="menu menu-horizontal">
               {/* Navbar menu content here */}
@@ -45,7 +45,7 @@ const Navbar = ({children}) => {
         {/* Page content here */}
         {children}
 
-        
+
       </div>
       <div className="drawer-side ">
         <label
