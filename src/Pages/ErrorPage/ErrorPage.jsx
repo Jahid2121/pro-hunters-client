be6e0a05f3651be3119ca2077df63e3e488c3233
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-05 04:51:02 +0600

    Change Website name and image in the url

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,11 @@
 import Lottie from "lottie-react";
 import four0four from "../../assets/animation/four0four.json"
+import { Link } from "react-router-dom";
 const ErrorPage = () => {
   return (
-    <div className="max-w-screen-md ml-32">
+    <div className="max-w-screen-sm relative mx-auto">
     <Lottie animationData={four0four}></Lottie>
+    <Link to="/"><button className="absolute right-[240px]  bg-customOrange px-5 py-3 rounded-full text-white bottom-16">Go Back To Home</button></Link>
     </div>
   );
 };
