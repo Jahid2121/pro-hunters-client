b35b7b53baddb0a773eb968f2cb56a3b47292d94
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-08 12:41:22 +0600

    Add card in the blogs page

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -1,28 +1,42 @@
+import { Card } from "flowbite-react";
 import { useEffect, useState } from "react";
 
 const Blogs = () => {
-    const [blogsData, setBlogsData] = useState([]);
-    console.log(blogsData);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('./Blogs.json'); // Path to the JSON file in the public folder
-        const data = await response.json();
-        setBlogsData(data);
-      } catch (error) {
-        console.error('Error fetching data: ', error);
-      }
-    };
 
-    fetchData();
-  }, []);
-
-
-  return (
-    <div>
-    <h2>  Welcome to Blogs </h2>
-    </div>
-  );
-};
-
-export default Blogs;
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch('./Blogs.json'); // Path to the JSON file in the public folder
+            const data = await response.json();
+            setBlogsData(data);
+          } catch (error) {
+            console.error('Error fetching data: ', error);
+          }
+        };
+    
+        fetchData();
+      }, []);
+      const [blogsData, setBlogsData] = useState([]);
+      console.log(blogsData);
+    return (
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 ">
+        {blogsData.map(blog => (
+          <Card key={blog.id} >
+            <img src={blog.imageUrl} alt="" />
+            <a>
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{blog.title}</h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{blog.description}</p>
+            <a  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">
+              Read more
+              <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+              </svg>
+            </a>
+          </Card>
+        ))}
+      </div>
+    );
+  };
+  
+  export default Blogs;
